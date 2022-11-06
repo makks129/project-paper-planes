27201f0d80bece1452884d5fb10b0c1ef252826f
Max Strelnikov <max.strelnikov@pipedrive.com>
2022-11-06 16:07:13 +0200

    mysql: added db to docker compose and connected to it from app

--- main.go
+++ main.go
@@ -4,7 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"gorm.io/driver/mysql"
+
+	"gorm.io/gorm"
 )
+
+var db *gorm.DB
+
+func setupDb() {
+	dsn := "ppp.user:ppp123@tcp(mysql:3306)/ppp?charset=utf8mb4&parseTime=True&loc=Local"
+	mysqlDb, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	println("Connected to db")
+	db = mysqlDb
+}
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
@@ -17,6 +33,9 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+	setupDb()
+
 	r := setupRouter()
+
 	r.Run(":9000")
 }
